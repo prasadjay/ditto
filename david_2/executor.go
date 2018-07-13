6362080a9341eeb9dda2696e0b024388e5c30f3b
Prasad Jayashanka <prasadjayashanka@gmail.com>
2018-07-13 11:14:57 +0530

    aaaa

--- david_2/executor.go
+++ david_2/executor.go
@@ -28,31 +28,31 @@ func main() {
 		fmt.Println(usr.HomeDir)
 	}
 
-	prog := os.Args[0]
+	InArguments := os.Args
+
+	prog := InArguments[0]
 
 	tmp := "/tmp/" + prog + "." + strconv.Itoa(os.Getpid())
-	_ = tmp
-	/*
-		tmp2 := "/tmp/." + prog + "." + strconv.Itoa(os.Getpid()) + ".2"
-		osuf := "." + prog + ".orig"
-		nsuf := "." + prog + ".1"
-		apt := "/usr/bin/apt-get"
-		nawk := "/usr/bin/nawk"
-		apoctl := "/usr/bin/apoctl"
-		enforcerd := "/usr/sbin/enforcerd"
-		systemctl := "/bin/systemctl"
-		awk := "/usr/bin/nawk"
-		swapoff := "/sbin/swapoff"
-		curl := "/usr/bin/curl"
-		id := "/usr/bin/id"
-		k := "/bin/ksh"
-		//ostype := `/bin/uname -a | $awk '{print$1" "substr($3,1,1)}'`
-		adminconf := "/etc/kubernetes/admin.conf"
-		config := usr.HomeDir + "/.kube/config"
-		kubeadm := "/usr/bin/kubeadm"
-		kubelet := "/usr/bin/kubelet"
-		kubectl := "/usr/bin/kubectl"
-	*/
+	tmp2 := "/tmp/." + prog + "." + strconv.Itoa(os.Getpid()) + ".2"
+	// osuf := "." + prog + ".orig"
+	// nsuf := "." + prog + ".1"
+	apt := "/usr/bin/apt-get"
+	nawk := "/usr/bin/nawk"
+	// apoctl := "/usr/bin/apoctl"
+	// enforcerd := "/usr/sbin/enforcerd"
+	// systemctl := "/bin/systemctl"
+	// awk := "/usr/bin/nawk"
+	swapoff := "/sbin/swapoff"
+	curl := "/usr/bin/curl"
+	id := "/usr/bin/id"
+	// k := "/bin/ksh"
+	// //ostype := `/bin/uname -a | $awk '{print$1" "substr($3,1,1)}'`
+	// adminconf := "/etc/kubernetes/admin.conf"
+	// config := usr.HomeDir + "/.kube/config"
+	// kubeadm := "/usr/bin/kubeadm"
+	// kubelet := "/usr/bin/kubelet"
+	// kubectl := "/usr/bin/kubectl"
+
 	log := "/var/tmp/" + prog + ".log.date " + nowtime.Format("2006-Mar-02 15:04:05")
 	out := "./Install_Readme_" + prog + ".log.date " + nowtime.Format("2006-Mar-02 15:04:05")
 	//version := "1.00"
@@ -80,10 +80,59 @@ func main() {
 		return
 	}
 
-	outBytes, err := exec.Command("/bin/ksh", "PATH=\"$HOME:/usr/bin:/bin:/usr/sbin:/sbin:/usr/ucb\";export PATH;! -x /usr/bin/curl", "ksh").Output()
+	outBytes, err := exec.Command("/bin/ksh", "PATH=\"$HOME:/usr/bin:/bin:/usr/sbin:/sbin:/usr/ucb\";export PATH;! -x "+curl, "ksh").Output()
 	if err != nil {
-		fmt.Println("Error : " + err.Error())
-	} else {
-		fmt.Println("Success : " + string(outBytes))
+		fmt.Println(prog + ": SANITY: /usr/bin/curl missing!  Will install later ...")
 	}
+
+	outBytes, err = exec.Command("/bin/ksh", "PATH=\"$HOME:/usr/bin:/bin:/usr/sbin:/sbin:/usr/ucb\";export PATH;! -x "+apt, "ksh").Output()
+	if err != nil {
+		os.Remove(tmp)
+		os.Remove(tmp2)
+		fmt.Println(prog + ": SANITY: /usr/bin/apt-get missing!  Quitting...")
+		os.Exit(1)
+	}
+
+	outBytes, err = exec.Command("/bin/ksh", "PATH=\"$HOME:/usr/bin:/bin:/usr/sbin:/sbin:/usr/ucb\";export PATH;! -x "+nawk, "ksh").Output()
+	if err != nil {
+		os.Remove(tmp)
+		os.Remove(tmp2)
+		fmt.Println(prog + ": SANITY: /usr/bin/nawk missing!  Quitting...")
+		os.Exit(1)
+	}
+
+	if len(InArguments) > 1 {
+		switch InArguments[1] {
+		default:
+			fmt.Println("usage : " + prog + " - no flags at this time.")
+			break
+		}
+	} else {
+		fmt.Println("usage : " + prog + " - no flags at this time.")
+	}
+
+	outBytes, err = exec.Command("/bin/ksh", "PATH=\"$HOME:/usr/bin:/bin:/usr/sbin:/sbin:/usr/ucb\";export PATH;! -x "+id, "ksh").Output()
+	if err != nil {
+		os.Remove(tmp)
+		os.Remove(tmp2)
+		fmt.Println(prog + ": SANITY: /usr/bin/id missing!  Quitting...")
+		os.Exit(1)
+	}
+
+	outBytes, err = exec.Command("/bin/ksh", "PATH=\"$HOME:/usr/bin:/bin:/usr/sbin:/sbin:/usr/ucb\";export PATH;! -x "+swapoff, "ksh").Output()
+	if err != nil {
+		os.Remove(tmp)
+		os.Remove(tmp2)
+		fmt.Println(prog + ": SANITY: /sbin/swapoff missing!  Quitting...")
+		os.Exit(1)
+	}
+
+	outBytes, err = exec.Command("/bin/ksh", "PATH=\"$HOME:/usr/bin:/bin:/usr/sbin:/sbin:/usr/ucb\";export PATH;! -x /bin/df", "ksh").Output()
+	if err != nil {
+		os.Remove(tmp)
+		os.Remove(tmp2)
+		fmt.Println(prog + ": SANITY: /bin/df missing!  Quitting...")
+		os.Exit(1)
+	}
+
 }
