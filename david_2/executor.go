ccf90153eff671bd7025b391ee3f12e1e5dc2b99
Prasad Jayashanka <prasadjayashanka@gmail.com>
2018-07-13 11:33:37 +0530

    a1

--- david_2/executor.go
+++ david_2/executor.go
@@ -139,8 +139,8 @@ func main() {
 
 	//APT-Related Requirements
 	apt_required_1 := [...]string{"ebtables", "ethtool"}
-	apt_required_2 := [...]string{"docker.io", "golang", "git", "apt-transport-https", "curl"}
-	k8s_required := [...]string{"kubelet", "kubeadm", "kubectl"}
+	//apt_required_2 := [...]string{"docker.io", "golang", "git", "apt-transport-https", "curl"}
+	//k8s_required := [...]string{"kubelet", "kubeadm", "kubectl"}
 
 	//Install apt-required
 
