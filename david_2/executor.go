bf0a2eb194be9c6e1220e6dd33e15845b9acc09b
Prasad Jayashanka <prasadjayashanka@gmail.com>
2018-07-13 15:26:57 +0530

    added base code

--- david_2/executor.go
+++ david_2/executor.go
@@ -21,20 +21,20 @@ func main() {
 	hostname, _ := os.Hostname()
 	nowtime := time.Now().UTC()
 	continueCommand := ""
+	homeDir := ""
 
 	usr, err := user.Current()
 	if err != nil {
 		fmt.Println("Error getting user for this session.")
 		return
 	} else {
-		fmt.Println(usr.HomeDir)
+		homeDir = usr.HomeDir
 	}
 
 	InArguments := os.Args
 
 	tokens := strings.Split(InArguments[0], "/")
 	prog := tokens[len(tokens)-1]
-	fmt.Println(prog)
 
 	tmp := "/tmp/" + prog + "." + strconv.Itoa(os.Getpid())
 	tmp2 := "/tmp/." + prog + "." + strconv.Itoa(os.Getpid()) + ".2"
@@ -151,7 +151,7 @@ func main() {
 
 	for x := 0; x < len(apt_required_1); x++ {
 		value := apt_required_1[x]
-		fmt.Println("Installing " + value + "...\\c")
+		fmt.Print("Installing " + value + "...\\c")
 
 		_, err = exec.Command("/bin/ksh", "PATH=\"$HOME:/usr/bin:/bin:/usr/sbin:/sbin:/usr/ucb\";export PATH;"+apt+" install -y "+value+" -q > /dev/null 2>&1", "ksh").Output()
 		if err != nil {
@@ -163,12 +163,12 @@ func main() {
 
 	//Perform APT Update
 	_, _ = exec.Command("/bin/ksh", "PATH=\"$HOME:/usr/bin:/bin:/usr/sbin:/sbin:/usr/ucb\";export PATH;"+apt+" update > /dev/null 2>&1", "ksh").Output()
-	fmt.Println("Done")
+	fmt.Println("Done updating apt")
 
 	//Install apt-required 2
 	for x := 0; x < len(apt_required_2); x++ {
 		value := apt_required_2[x]
-		fmt.Println("Installing " + value + "...\\c")
+		fmt.Print("Installing " + value + "...\\c")
 
 		_, err = exec.Command("/bin/ksh", "PATH=\"$HOME:/usr/bin:/bin:/usr/sbin:/sbin:/usr/ucb\";export PATH;"+apt+" install -y "+value+" -q > /dev/null 2>&1", "ksh").Output()
 		if err != nil {
@@ -181,13 +181,13 @@ func main() {
 	//Restart Docker
 	_, err = exec.Command("/bin/ksh", "PATH=\"$HOME:/usr/bin:/bin:/usr/sbin:/sbin:/usr/ucb\";export PATH;systemctl start docker >/dev/null 2>&1 ; sleep 2;$systemctl enable docker >/dev/null 2>&1 ; sleep 2", "ksh").Output()
 	if err != nil {
-		fmt.Println("Error : " + err.Error())
+		fmt.Println("Error restarting dockers : " + err.Error())
 	} else {
-		fmt.Println("Done")
+		fmt.Println("Done restarting dockers")
 	}
 
 	//Adding apt-key
-	fmt.Println("Adding apt-key...\\c")
+	fmt.Print("Adding apt-key...\\c")
 	_, err = exec.Command("/bin/ksh", "PATH=\"$HOME:/usr/bin:/bin:/usr/sbin:/sbin:/usr/ucb\";export PATH;curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - >/dev/null 2>&1", "ksh").Output()
 	if err != nil {
 		fmt.Println("Error : " + err.Error())
@@ -203,21 +203,21 @@ func main() {
 	//execute verifyKLists
 	_, err = exec.Command("/bin/ksh", "PATH=\"$HOME:/usr/bin:/bin:/usr/sbin:/sbin:/usr/ucb\";export PATH;./verifyKList.sh", prog, tmp, tmp2, "ksh").Output()
 	if err != nil {
-		fmt.Println("Error : " + err.Error())
+		fmt.Println("Error verify Kubernetes list : " + err.Error())
 	} else {
-		fmt.Println("Done")
+		fmt.Println("Done verifying Kubernetes list")
 	}
 	//delete verifyList.sh
 	os.Remove("verifyKList.sh")
 
 	//Perform APT Update
 	_, _ = exec.Command("/bin/ksh", "PATH=\"$HOME:/usr/bin:/bin:/usr/sbin:/sbin:/usr/ucb\";export PATH;"+apt+" update > /dev/null 2>&1", "ksh").Output()
-	fmt.Println("Done")
+	fmt.Println("Done updating apt")
 
 	//Install K8S Required
 	for x := 0; x < len(k8s_required); x++ {
 		value := k8s_required[x]
-		fmt.Println("Installing " + value + "...\\c")
+		fmt.Print("Installing " + value + "...\\c")
 
 		_, err = exec.Command("/bin/ksh", "PATH=\"$HOME:/usr/bin:/bin:/usr/sbin:/sbin:/usr/ucb\";export PATH;"+apt+" install -y "+value+" -q > /dev/null 2>&1", "ksh").Output()
 		if err != nil {
@@ -254,8 +254,8 @@ func main() {
 	}
 
 	//Clone cri-tools repo
-	fmt.Println("Cloning git repo for cri-tools...\\c")
-	/*_, err = exec.Command("/bin/ksh", "PATH=\"$HOME:/usr/bin:/bin:/usr/sbin:/sbin:/usr/ucb\";export PATH;git clone https://github.com/kubernetes-incubator/cri-tools.git >/dev/null 2>&1").Output()
+	/*fmt.Print("Cloning git repo for cri-tools...\\c")
+	_, err = exec.Command("/bin/ksh", "PATH=\"$HOME:/usr/bin:/bin:/usr/sbin:/sbin:/usr/ucb\";export PATH;git clone https://github.com/kubernetes-incubator/cri-tools.git >/dev/null 2>&1").Output()
 	if err != nil {
 		fmt.Println("Error fetching from GIT : " + err.Error())
 		os.Exit(1)
@@ -272,11 +272,144 @@ func main() {
 	//execute
 	_, err = exec.Command("/bin/ksh", "PATH=\"$HOME:/usr/bin:/bin:/usr/sbin:/sbin:/usr/ucb\";export PATH;./readme.sh", out, "ksh").Output()
 	if err != nil {
+		fmt.Println("Error creating readme : " + err.Error())
+	} else {
+		fmt.Println("Done creating readme")
+	}
+	//delete readme.sh
+	os.Remove("readme.sh")
+
+	//Create a network
+
+	cidr := ""
+	fmt.Print("For our cluster, please enter a CIDR address (example: 192.168.1.1/24): ")
+	_, _ = fmt.Scanln(&cidr)
+	fmt.Print("Kubeadm Init with " + cidr + " starting and key writeout...")
+	_, err = exec.Command("/bin/ksh", "PATH=\"$HOME:/usr/bin:/bin:/usr/sbin:/sbin:/usr/ucb\";export PATH;kubeadm init --pod-network-cidr=$"+cidr+" >> $out", "ksh").Output()
+	if err != nil {
 		fmt.Println("Error : " + err.Error())
 	} else {
 		fmt.Println("Done")
 	}
-	//delete readme.sh
-	//os.Remove("readme.sh")
 
+	//Setup $HOME
+	fmt.Print("Kubeadm Init with " + cidr + " starting and key writeout...")
+	_, err = exec.Command("/bin/ksh", "PATH=\"$HOME:/usr/bin:/bin:/usr/sbin:/sbin:/usr/ucb\";export PATH;kubeadm init --pod-network-cidr=$"+cidr+" >> $out", "ksh").Output()
+	if err != nil {
+		fmt.Println("Error : " + err.Error())
+	} else {
+		fmt.Println("Done")
+	}
+
+	_, err = os.Stat(homeDir + "/.kube")
+
+	// See if directory exists.
+	if os.IsNotExist(err) {
+		//exists
+		_, err = exec.Command("/bin/ksh", "PATH=\"$HOME:/usr/bin:/bin:/usr/sbin:/sbin:/usr/ucb\";export PATH;mkdir -p $HOME/.kube", "ksh").Output()
+		if err != nil {
+			fmt.Println("Error creating .kube folder: " + err.Error())
+		} else {
+			fmt.Println("Done")
+		}
+		_, err = exec.Command("/bin/ksh", "PATH=\"$HOME:/usr/bin:/bin:/usr/sbin:/sbin:/usr/ucb\";export PATH;cp -i /etc/kubernetes/admin.conf $HOME/.kube/config", "ksh").Output()
+		if err != nil {
+			fmt.Println("Error copying kubernetes admin config: " + err.Error())
+		} else {
+			fmt.Println("Done")
+		}
+		_, err = exec.Command("/bin/ksh", "PATH=\"$HOME:/usr/bin:/bin:/usr/sbin:/sbin:/usr/ucb\";export PATH;chown $("+id+" -u):$("+id+" -g) $HOME/.kube/config", "ksh").Output()
+		if err != nil {
+			fmt.Println("Error chown " + id + " to user and group : " + err.Error())
+		} else {
+			fmt.Println("Done")
+		}
+		//Deploy Project Calico
+		_, err = exec.Command("/bin/ksh", "PATH=\"$HOME:/usr/bin:/bin:/usr/sbin:/sbin:/usr/ucb\";kubectl apply -f https://docs.projectcalico.org/v2.6/getting-started/kubernetes/installation/hosted/kubeadm/1.6/calico.yaml", "ksh").Output()
+		if err != nil {
+			fmt.Println("Error chown " + id + " to user and group : " + err.Error())
+		} else {
+			fmt.Println("Done")
+		}
+	} else {
+		//skip if Exists
+		fmt.Println(prog + " : " + homeDir + "/.kube exists... skipping")
+	}
+
+	//Untaint the Master
+	fmt.Print("Untaint of the master so it will be available for scheduling workloads...")
+	_, err = exec.Command("/bin/ksh", "PATH=\"$HOME:/usr/bin:/bin:/usr/sbin:/sbin:/usr/ucb\";kubectl taint nodes --all node-role.kubernetes.io/master- >/dev/null 2>&1", "ksh").Output()
+	if err != nil {
+		fmt.Println("Error untaint master " + id + " to user and group : " + err.Error())
+	} else {
+		fmt.Println("Done")
+	}
+
+	//Deploy application
+
+	app_r := ""
+	fmt.Print("Ready to deploy an application? [y/n] : ")
+	_, _ = fmt.Scanln(&app_r)
+
+	//Take users input
+
+	app_r = strings.ToLower(app_r)
+
+	if app_r == "y" {
+		fmt.Print("Creating Namespace....")
+		_, err = exec.Command("/bin/ksh", "PATH=\"$HOME:/usr/bin:/bin:/usr/sbin:/sbin:/usr/ucb\";kubectl create namespace guestbook", "ksh").Output()
+		if err != nil {
+			fmt.Println("Error : " + err.Error())
+		} else {
+			fmt.Println("Done")
+		}
+
+		fmt.Print("Grabbing application....")
+		_, err = exec.Command("/bin/ksh", "PATH=\"$HOME:/usr/bin:/bin:/usr/sbin:/sbin:/usr/ucb\";kubectl apply -n guestbook -f \"https://raw.githubusercontent.com/dnester/guestbook/master/guestbook.yaml\"", "ksh").Output()
+		if err != nil {
+			fmt.Println("Error : " + err.Error())
+		} else {
+			fmt.Println("Done")
+		}
+	} else if app_r == "n" {
+		fmt.Print("Creating Namespace....")
+		_, err = exec.Command("/bin/ksh", "PATH=\"$HOME:/usr/bin:/bin:/usr/sbin:/sbin:/usr/ucb\";kubectl create namespace guestbook", "ksh").Output()
+		if err != nil {
+			fmt.Println("Error : " + err.Error())
+		} else {
+			fmt.Println("Done")
+		}
+
+		fmt.Print("Grabbing application....")
+		_, err = exec.Command("/bin/ksh", "PATH=\"$HOME:/usr/bin:/bin:/usr/sbin:/sbin:/usr/ucb\";kubectl apply -n guestbook -f \"https://raw.githubusercontent.com/dnester/guestbook/master/guestbook.yaml\"", "ksh").Output()
+		if err != nil {
+			fmt.Println("Error : " + err.Error())
+		} else {
+			fmt.Println("Done")
+		}
+	} else {
+		fmt.Println("Invalid selection, only Y or N is allowed.")
+		os.Exit(1)
+	}
+
+	//At this point you should have a fully-functional kubernetes cluster on which you can run workloads.
+	//Final message.
+
+	fmt.Println("")
+	fmt.Println("")
+	fmt.Println("The K8 cluster is now coming up.  Please test with the following command:")
+	fmt.Println("")
+	fmt.Println("         $kubectl get pods --all-namespaces")
+	fmt.Println("")
+	fmt.Println("")
+	fmt.Println(prog + " run on `/bin/hostname` completed on " + nowtime.Format("2006-Mar-02 15:04:05") + ". Be sure to review the installation readme in " + out)
+	fmt.Println("")
+
+	os.Remove(tmp)
+	os.Remove(tmp2)
+	os.Exit(0)
+
+	//============================================================================
+	// 	END of dk8s
+	//============================================================================
 }
